622a0684680ed40f17ff941ce8ca275a85e1c0db
Katalina T <git@kat.cafe>
2019-02-07 04:36:09 -0600

    01: consolidate

--- 03-in-a-window-darkly/premake5.lua
+++
@@ -1,7 +0,0 @@
-workspace "GFXAdventures"
-  project "03-In-A-Window-Darkly"
-    kind "ConsoleApp"
-    language "C++"
-    includedirs "../includes"
-    files { "**.h", "**.cpp" }
-    targetdir ".bin/"
